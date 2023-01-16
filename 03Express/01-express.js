9fae56a17f4f3a750c22d2d0c4afa6a413d86987
javiprada1 <javiprada1@gmail.com>
2023-01-16 17:30:43 +0100

    Add Mongoose and others

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3005
+
+require('dotenv').config()
+const port = process.env.PORT||3005
+
+//Conexión a la Base de Datos
+//Conexión a base de datos
+const mongoose = require('mongoose');
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mu1fu4x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
 
 
 //Motor de plantillas
